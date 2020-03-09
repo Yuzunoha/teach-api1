d6c3b7089e578ad7f47e84a05e5562201af70a46
Yuzunoha <studysteadystandby@gmail.com>
2020-03-09 13:44:26 +0900

    メソッドのガード実装

--- api/v1/sign_up.php
+++ api/v1/sign_up.php
@@ -1,6 +1,31 @@
 <?php
 
+/*
+[ユーザー登録]
+POST: https://teachapi.herokuapp.com/sign_up
+パラメーターが
+body parameterで
+{
+sign_up_user_params: {
+name: name,
+bio: bio,
+email: email,
+password: password,
+password_confirmation: passwordConfirmation
+}
+}
+ */
+
 require_once './util.php';
+
+// postかどうか
+if ('post' !== strtolower($_SERVER["REQUEST_METHOD"])) {
+    /* postじゃない */
+    util::sendResponse([
+        'error' => 'メソッドがpostじゃない',
+    ]);
+    exit();
+}
 
 $o = util::getRequestJsonObj();
 

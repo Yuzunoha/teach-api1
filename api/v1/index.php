d41c1557702f9ac2bac8766215c2e9c32a80fafd
Yuzunoha <studysteadystandby@gmail.com>
2020-03-10 22:54:07 +0900

    currentDirName取得成功

--- api/v1/index.php
+++ api/v1/index.php
@@ -2,6 +2,7 @@
 
 require_once './util.php';
 
+$currentDirName = array_pop(explode('/', dirname(__FILE__)));
 $url = $_SERVER['REQUEST_URI'];
 $path = util::getPathArray($url);
 $qs = util::getQSDict($url);
@@ -9,21 +10,7 @@ $ret = [
     'url' => $url,
     'path' => $path,
     'qs' => $qs,
+    'dirname' => $currentDirName
 ];
 echo "<pre>";
 print_r($ret);
-// util::sendResponse($ret);
-
-/* routing */
-switch (route) {
-    case 'sign_up':
-        break;
-    case 'sign_in':
-        break;
-    case 'users':
-        break;
-    case 'posts':
-        break;
-    default:
-        /* DO NOTHING */
-}

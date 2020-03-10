c3513a874e5f5e54748c53c8544a1dd44bb6e8ca
Yuzunoha <studysteadystandby@gmail.com>
2020-03-10 19:20:23 +0900

    微妙に開発した。帰ろう

--- api/v1/index.php
+++ api/v1/index.php
@@ -6,8 +6,24 @@ $url = $_SERVER['REQUEST_URI'];
 $path = util::getPathArray($url);
 $qs = util::getQSDict($url);
 $ret = [
-  'url' => $url,
-  'path' => $path,
-  'qs' => $qs
+    'url' => $url,
+    'path' => $path,
+    'qs' => $qs,
 ];
-util::sendResponse($ret);
+echo "<pre>";
+print_r($ret);
+// util::sendResponse($ret);
+
+/* routing */
+switch (route) {
+    case 'sign_up':
+        break;
+    case 'sign_in':
+        break;
+    case 'users':
+        break;
+    case 'posts':
+        break;
+    default:
+        /* DO NOTHING */
+}

4eae9b8fbee4ca6e04208d914b1c230bfde08eb6
Yuzunoha <studysteadystandby@gmail.com>
2020-03-11 12:34:06 +0900

    整理したっぽい

--- api/v1/index.php
+++ api/v1/index.php
@@ -2,15 +2,11 @@
 
 require_once './util.php';
 
-$currentDirName = array_pop(explode('/', dirname(__FILE__)));
-$url = $_SERVER['REQUEST_URI'];
-$path = util::getPathArray($url);
-$qs = util::getQSDict($url);
+$path = util::getPathArray();
+$qs = util::getQSDict();
 $ret = [
-    'url' => $url,
     'path' => $path,
     'qs' => $qs,
-    'dirname' => $currentDirName
 ];
 echo "<pre>";
 print_r($ret);

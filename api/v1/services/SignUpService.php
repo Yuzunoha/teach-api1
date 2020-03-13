fdcc75e1c3ca3264e0bad4f352c42ebf45acb53a
Yuzunoha <studysteadystandby@gmail.com>
2020-03-13 16:26:41 +0900

    未入力ガード

--- api/v1/services/SignUpService.php
+++ api/v1/services/SignUpService.php
@@ -15,16 +15,34 @@ class SignUpService
         password: ""
         password_confirmation: ""
          */
-        $signUpUserParams = $body['sign_up_user_params'];
+        $name = $body['sign_up_user_params']['name'];
+        $bio = $body['sign_up_user_params']['bio'];
+        $email = $body['sign_up_user_params']['email'];
+        $password = $body['sign_up_user_params']['password'];
+        $passwordConfirmation = $body['sign_up_user_params']['password_confirmation'];
+
+        // 未入力ガード
+        if (!isset($name)) {
+            return new YuzunohaSnsError('nameを入力してください', 400);
+        }
+        if (!isset($bio)) {
+            return new YuzunohaSnsError('bioを入力してください', 400);
+        }
+        if (!isset($email)) {
+            return new YuzunohaSnsError('emailを入力してください', 400);
+        }
+        if (!isset($password)) {
+            return new YuzunohaSnsError('passwordを入力してください', 400);
+        }
 
         // パスワード確認入力ガード
-        if ($signUpUserParams['password'] !== $signUpUserParams['password_confirmation']) {
+        if ($password !== $passwordConfirmation) {
             /* パスワード確認入力不一致 */
             return new YuzunohaSnsError('パスワードと確認入力が合致しません', 400);
         }
 
         // email重複ガード
-        if (1 <= count(YuzunohaSnsUser::selectWhereEmail($signUpUserParams['email']))) {
+        if (1 <= count(YuzunohaSnsUser::selectWhereEmail($email))) {
             /* email重複 */
             return new YuzunohaSnsError('そのemailは既に登録されています', 400);
         }

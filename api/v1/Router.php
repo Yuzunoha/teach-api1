2bfb9f7d7f3feb3dcf4ab665ff51eb98d436c88f
Yuzunoha <studysteadystandby@gmail.com>
2020-03-13 19:41:24 +0900

    ユーザーズ

--- api/v1/Router.php
+++ api/v1/Router.php
@@ -3,6 +3,7 @@
 require_once dirname(__FILE__) . '/services/util.php';
 require_once dirname(__FILE__) . '/controllers/SignUpController.php';
 require_once dirname(__FILE__) . '/controllers/SignInController.php';
+require_once dirname(__FILE__) . '/controllers/UsersController.php';
 require_once dirname(__FILE__) . '/controllers/CommonController.php';
 
 class Router
@@ -21,6 +22,11 @@ class Router
         if ('sign_in' === $path[0]) {
             if ('POST' === $method) {
                 SignInController::post();
+            }
+        }
+        if ('users' === $path[0]) {
+            if ('GET' === $method) {
+                UsersController::get();
             }
         }
 

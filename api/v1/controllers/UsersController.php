5289f0ca82ea1e99be7451a92ae03450439fd1f1
Yuzunoha <studysteadystandby@gmail.com>
2020-03-13 20:03:16 +0900

    閉店

--- api/v1/controllers/UsersController.php
+++ api/v1/controllers/UsersController.php
@@ -1,11 +1,16 @@
 <?php
 
 require_once dirname(__FILE__) . '/../services/util.php';
+require_once dirname(__FILE__) . '/../services/UsersService.php';
 
 class UsersController
 {
     public static function get()
     {
-        util::sendResponse("ユーザーズコントローラ");
+        $qsDict = util::getQSDict();
+        $tokenFromRequestHeader = util::getTokenFromRequestHeader();
+
+        $result = UsersService::getUsers($tokenFromRequestHeader, $qsDict);
+        util::sendResponse($tokenFromRequestHeader);
     }
 }

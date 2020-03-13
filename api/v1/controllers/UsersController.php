0c91bb49deee561ce6e6fb09f29fa0a53cb20fe1
Yuzunoha <studysteadystandby@gmail.com>
2020-03-14 01:39:08 +0900

    秘匿データを落とした

--- api/v1/controllers/UsersController.php
+++ api/v1/controllers/UsersController.php
@@ -13,6 +13,10 @@ class UsersController
         /* 認証OK */
         $qsDict = util::getQsDict();
         $users = UsersService::get($qsDict);
+        foreach ($users as &$user) {
+            unset($user['password_hash']);
+            unset($user['token']);
+        }
         util::sendResponse($users);
     }
 }

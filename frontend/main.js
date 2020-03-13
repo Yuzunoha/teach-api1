db9cdad533045bad3ef67edfe29fccde55d97de1
Yuzunoha <studysteadystandby@gmail.com>
2020-03-13 14:06:38 +0900

    url変えた

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
-const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
-const urlUsers = 'https://teachapi.herokuapp.com/users';
-const urlPosts = 'https://teachapi.herokuapp.com/posts';
+const baseUrl = 'http://localhost:10080/yuzunoha/teach-api1/';
+const urlSignUp = baseUrl + 'sign_up';
+const urlSignIn = baseUrl + 'sign_in';
+const urlUsers = baseUrl + 'users';
+const urlUsers = baseUrl + 'posts';
 
 const updateProfileTagByLocalStorage = () => {
   const profileTag = document.getElementById('profile');
